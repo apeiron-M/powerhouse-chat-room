a2d4da172a077a63ffdbd3f4dd88f63c6356af28
apeironmaker <123967815+apeiron-M@users.noreply.github.com>
2024-11-28 09:57:18 +0100

    initial chat-room commit

---
+++ document-models/chat-room/src/reducers/add-message.ts
@@ -0,0 +1,18 @@
+/**
+ * This is a scaffold file meant for customization:
+ * - modify it by implementing the reducer functions
+ * - delete the file and run the code generator again to have it reset
+ */
+
+import { ChatRoomAddMessageOperations } from "../../gen/add-message/operations";
+
+export const reducer: ChatRoomAddMessageOperations = {
+  addMessageOperation(state, action, dispatch) {
+    // TODO: Implement "addMessageOperation" reducer
+    throw new Error('Reducer "addMessageOperation" not yet implemented');
+  },
+  addEmojiReactionOperation(state, action, dispatch) {
+    // TODO: Implement "addEmojiReactionOperation" reducer
+    throw new Error('Reducer "addEmojiReactionOperation" not yet implemented');
+  },
+};
